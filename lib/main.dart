53588e6aab16da57c15608481a754b67ca4a8ed7
itsrealoj <stephenojwang1040@gmail.com>
2021-02-08 20:04:58 +0300

    added an icon

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class RealCard extends StatelessWidget {
     return Scaffold(
         backgroundColor: Colors.grey[900],
         appBar: AppBar(
-          title: Text('Real ID Card'),
+          title: Text('Real NINJA ID Card'),
           centerTitle: true,
           backgroundColor: Colors.grey[850],
           elevation: 0.0,
@@ -30,7 +30,30 @@ class RealCard extends StatelessWidget {
                       fontSize: 20.0,
                       fontWeight: FontWeight.bold)),
               SizedBox(
+                height: 25.0,
+              ),
+
+              Text('CURRENT NINJA LEVEL',
+                  style: TextStyle(color: Colors.grey, letterSpacing: 2.0)),
+              SizedBox(
                 height: 10.0,
+              ),
+              Text('10',
+                  style: TextStyle(
+                      color: Colors.amberAccent[200],
+                      letterSpacing: 2.0,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold)),
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey[400],
+                  ),
+                ],
               ),
             ],
           ),
